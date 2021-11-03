6f4d95e322bc1d2f3826e0d0c5c0fadfac27e3e9
Raushan kumar Giri <raushangiri.raj@gmail.com>
2021-11-03 08:55:30 +0530

    login and registration

---
+++ src/components/Login/Login.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import './Login.css';
+import Frontimage from '../Images/frontImg.jpg'
+import backImg from '../Images/backImg.jpg'
+
+function Login() {
+    return (
+        <>
+            <div className="container">
+                <input type="checkbox" id="flip" />
+                <div className="cover">
+                    <div className="front">
+                        <img src={Frontimage} alt="...." />
+                        <div className="text">
+                            <span className="text-1">Every new friend is a <br /> new adventure</span>
+                            <span className="text-2">Let's get connected</span>
+                        </div>
+                    </div>
+                    <div className="back">
+                        <img className="backImg" src={backImg} alt="...." />
+                        <div className="text">
+                            <span className="text-1">Complete miles of journey <br /> with one step</span>
+                            <span className="text-2">Let's get started</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="forms">
+                    <div className="form-content">
+                        <div className="login-form">
+                            <div className="title">Login</div>
+                            <form action="#">
+                                <div className="input-boxes">
+                                    <div className="input-box">
+                                        <i className="fas fa-envelope"></i>
+                                        <input type="text" placeholder="Enter Your Email" required />
+                                    </div>
+                                    <div className="input-box">
+                                        <i className="fas fa-lock"></i>
+                                        <input type="password" placeholder="Enter your Password" required />
+                                    </div>
+                                    <div className="text"><a href="/">Forgot password?</a></div>
+                                    <div className="button input-box">
+                                        <input type="submit" value="Login" />
+                                    </div>
+                                    <div className="text sign-up-text">Don't have an account? <label for="flip">Sigup now</label></div>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="signup-form">
+                            <div className="title">Signup</div>
+                            <form action="#">
+                                <div className="input-boxes">
+                                    <div className="input-box">
+                                        <i className="fas fa-user"></i>
+                                        <input type="text" placeholder="Enter your name" required />
+                                    </div>
+                                    <div className="input-box">
+                                        <i className="fas fa-envelope"></i>
+                                        <input type="text" placeholder="Enter your email" required />
+                                    </div>
+                                    <div className="input-box">
+                                        <i className="fas fa-lock"></i>
+                                        <input type="password" placeholder="Enter your password" required />
+                                    </div>
+                                    <div className="button input-box">
+                                        <input type="submit" value="Create Account" />
+                                    </div>
+                                    <div className="text sign-up-text">Already have an account? <label for="flip">Login now</label></div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login
